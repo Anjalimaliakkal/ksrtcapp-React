d171e1cd1805267399104d6dde25c118a2acae51
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-13 21:51:10 +0530

    signup commited

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
      <Route path='/' element={<SignIn/>}/>
+     <Route path='/SignUp' element={<SignUp/>}/>
     </Routes>
     </BrowserRouter>
   );
