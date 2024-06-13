defb3730f94a50ff7ffc9523c7670b5cb857e4ee
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-13 22:07:45 +0530

    addbus created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
      <Route path='/' element={<SignIn/>}/>
      <Route path='/SignUp' element={<SignUp/>}/>
+     <Route path='/dashboard' element={<Dashboard/>}/>
     </Routes>
     </BrowserRouter>
   );
